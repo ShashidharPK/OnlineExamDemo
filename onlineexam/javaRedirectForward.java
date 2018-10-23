20026b2e3688ffdd1bf29acd63dc6b55f901188f
ShashidharPK <33952012+ShashidharPK@users.noreply.github.com>
2018-10-23 14:39:46 +0000

    Update javaRedirectForward.java

--- onlineexam/javaRedirectForward.java
+++ onlineexam/javaRedirectForward.java
@@ -1,5 +1,6 @@
 <%
 	String redirectURL = "http://www.example.com/";
+tring redirectURL = "http://www.example.com/";
 	response.sendRedirect(redirectURL);
 	response.sendForward(redirectURL);
 	return;
